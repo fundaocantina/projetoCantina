842b61e2364deafff611381a093fc13568303ad0
Rebeca B. Santos <rebecabuenos17@gmail.com>
2026-08-11 15:46:55 -0300

    Produto
    Gets e Sets

--- src/main/java/com/senai/projetoCantina/model/Produto.java
+++ src/main/java/com/senai/projetoCantina/model/Produto.java
@@ -5,5 +5,26 @@ public class Produto {
 	private Long id;
 	private Categoria id_categoria;
 	private String nome;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	//=============================
+	public Categoria getId_categoria() {
+		return id_categoria;
+	}
+	public void setId_categoria(Categoria id_categoria) {
+		this.id_categoria = id_categoria;
+	}
+	//=============================
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 }
