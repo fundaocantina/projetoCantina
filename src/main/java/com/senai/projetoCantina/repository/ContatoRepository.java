dcc1e7f70400f560b821569e7290044fec6855da
HexMortem <rebecabuenos17@gmail.com>
2026-08-21 16:55:31 -0300

    Criação Repository

---
+++ src/main/java/com/senai/projetoCantina/repository/ContatoRepository.java
@@ -0,0 +1,8 @@
+package com.senai.projetoCantina.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.senai.projetoCantina.model.Contato;
+
+public interface ContatoRepository extends JpaRepository<Contato, Long>{
+
+}
---
+++ src/main/java/com/senai/projetoCantina/repository/TipoClienteRepository.java
@@ -0,0 +1,8 @@
+package com.senai.projetoCantina.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.senai.projetoCantina.model.TipoCliente;
+
+public interface TipoClienteRepository extends JpaRepository<TipoCliente, Long>{
+
+}
