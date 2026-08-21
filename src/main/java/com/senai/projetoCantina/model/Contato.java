e1060f67dbdc1d2d1806aba465a944c05ee03598
HexMortem <rebecabuenos17@gmail.com>
2026-08-21 16:47:39 -0300

    Add Entidades
    Arrumando: Cliente, Contato e TipoCliente

--- src/main/java/com/senai/projetoCantina/model/Contato.java
+++ src/main/java/com/senai/projetoCantina/model/Contato.java
@@ -1,5 +1,76 @@
 package com.senai.projetoCantina.model;
 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "contato")
 public class Contato {
+	
+	private Long id;
+	private String telefone;
+	private String email;
+	@ManyToOne
+	@JoinColumn(name = "id_cliente")
+	private Cliente cliente;
+	@ManyToOne
+	@JoinColumn(name = "id_funcionario")
+	private Funcionario funcionario;
+	
+	public Contato() {
+	}
+	
+	//=============================
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	//=============================
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	//=============================
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	//=============================
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	//=============================
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	//=============================
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contato contato = (Contato) o;
+        return Objects.equals(id, contato.id);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 
 }
